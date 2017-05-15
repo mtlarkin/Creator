f167fbb77cc18808e3f0fac8a7d119ce63658fa8
Marc Larkin <Larkimarc@gmail.com>
2017-05-15 09:14:32 -0700

    Add db context

---
+++ src/Creator/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Creator.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+       
+    }
+}
---
+++ src/Creator/Models/CreatorDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creator.Models
+{
+    public class CreatorDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CreatorDbContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
