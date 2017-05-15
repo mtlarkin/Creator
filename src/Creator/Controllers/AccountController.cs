9fb81f30d26f08501e86a8ca7ffa1bfc03bede54
Marc Larkin <Larkimarc@gmail.com>
2017-05-15 10:49:25 -0700

    Add thorough documentation to the Account Controller

--- src/Creator/Controllers/AccountController.cs
+++ src/Creator/Controllers/AccountController.cs
@@ -17,9 +17,15 @@ namespace Creator.Controllers
         private readonly CreatorDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
+        /// <summary>
         ///Use Dependency Injection to connect with the database and instantiate the user manager and sign-in manager. 
-        ///UserManager works with the User table and SignInManager authenticates sign-in/sign-out
+        ///UserManager works with the User table
+        ///SignInManager authenticates sign-in/sign-out
+        /// </summary>
+        /// <param name="db">Connects to the database</param>
+        /// <param name="signInManager">Injects SignInManager to allow login/logout functionality</param>
+        /// <param name="userManager">Injects UserManager to allow user updating, creation, and deletion</param>
+        /// <returns>None</returns>
         public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, CreatorDbContext db)
         {
             _userManager = userManager;
@@ -27,20 +33,28 @@ namespace Creator.Controllers
             _db = db;
         }
 
-
+        ///<summary>
         /// GET: Index of AccountController - Returns the 'Index' page which will allow you to Register an account, login or logout
+        ///</summary>
+        ///<returns>Index View</returns>
         public IActionResult Index()
         {
             return View();
         }
-
+        /// <summary>
         /// GET: Register of AccountController
+        /// </summary>
+        /// <returns>Register View</returns>
         public IActionResult Register()
         {
             return View();
         }
 
+        ///<summary>
         /// POST: Register of AccountController - Take information from the Submitted RegisterViewModel ('model') and use it to create a new ApplicationUser via Identity
+        ///</summary>
+        ///<param name="model">Information received from the 'Register' view as a RegisterViewModel object</param>
+        ///<returns>Success: Account/Index; Error: Account/Register</returns>
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
@@ -55,13 +69,19 @@ namespace Creator.Controllers
                 return View();
             }
         }
-
+        /// <summary>
         /// GET: Login of AccountController
+        /// </summary>
+        /// <returns>Login view</returns>
         public IActionResult Login()
         {
             return View();
         }
+        ///<summary>
         /// POST: Login - Take information from the submitted LoginViewModel and send it to the Identity SignInManager. If the Username and Password are correct the page will redirect to the Index page. The page will reload if the incorrect information is added.
+        ///</summary>
+        ///<param name="model">Information received from the 'Login' view as a LoginViewModel object</param>
+        ///<returns>Success: Account/Index; Error: Account/Login</returns>
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
@@ -75,8 +95,10 @@ namespace Creator.Controllers
                 return View();
             }
         }
-
+        ///<summary>
         ///POST: Log off of AccountController - Send SignInManager the command to sign out of the current user's account 
+        ///</summary>
+        ///<returns>Account/Index with no current user</returns>
         [HttpPost]
         public async Task<IActionResult> LogOut()
         {
