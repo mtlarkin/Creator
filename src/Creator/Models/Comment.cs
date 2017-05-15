7839620efa3c88f3649900f111dd30ea5d3e6cbe
Marc Larkin <Larkimarc@gmail.com>
2017-05-15 12:33:33 -0700

    add Post and Comment models

---
+++ src/Creator/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creator.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Body { get; set; }
+        public int Score { get; set; }
+        public int Bump { get; set; }
+        public int Knock { get; set; }
+        public virtual Comment ParentComment { get; set; }
+        public virtual ApplicationUser CommentOwner { get; set; }
+
+    }
+}
--- src/Creator/Models/Post.cs
+++ src/Creator/Models/Post.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Creator.Models
 {
+    [Table("Posts")]
     public class Post
     {
+        [Key]
+        public int PostId { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Views { get; set; }
+        public string Link { get; set; }
+        public int Bumps { get; set; }
+        public int Knocks { get; set; }
+        public virtual ApplicationUser PostOwner { get; set; }
+        public virtual Comment PostComment { get; set; }
     }
 }
